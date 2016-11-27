becfeb4b4591d1bbf82e072f7d9ce2af8b72e24e
waynezheng <zwee.cn@gmail.com>
2016-11-27 12:38:53 +0800

    auto backup

---
+++ lnmp-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd
+if [ ! -d pkg ]; then
+    mkdir pkg
+fi
+cd pkg
+
+# git clone https://github.com/cuber/ngx_http_google_filter_module
+# git clone https://github.com/yaoweibin/ngx_http_substitutions_filter_module
+# wget http://soft.vpser.net/lnmp/lnmp1.3-full.tar.gz
+# tar -zxvf lnmp1.3-full.tar.gz
+cd lnmp1.3-full
+
